1c7d4f8ddf993c9eae9824b5b3e1981894779930
Dewitt Steward <dewitt.steward@me.com>
2026-03-20 11:17:21 -0500

    updating live session

--- letsplaybingo.classic-main/public/config.js
+++ letsplaybingo.classic-main/public/config.js
@@ -1,6 +1,6 @@
 window.LPB_CONFIG = Object.assign(
 	{
-		sessionApiUrl: 'https://www.dlbhfamily.com/wp-json/lpb/v1/session'
+		sessionApiUrl: 'https://dlbhfamily.com/wp-json/lpb/v1/session'
 	},
 	window.LPB_CONFIG || {}
 );
